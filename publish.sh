00e2292721d6df7ebd8cc512ca8b1cb3ab69293f
Quentin Bazin <quent42340@gmail.com>
2019-03-29 10:11:09 +0100

    Url changed.

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ git commit -am "$1"
 git push origin blog-source
 cd _site/
 rm -f CNAME
-echo "blog.gnidmoo.tk" > CNAME
+echo "blog.unarelith.net" > CNAME
 git add .
 git commit -am "$1"
 git push origin master
